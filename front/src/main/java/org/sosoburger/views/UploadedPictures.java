aeb1bea225b2f3ccfc33e64f75f96a07fe2fe019
Ixor <dsv>
2023-06-14 13:58:44 +0300

    пофиксил прэколы

--- front/src/main/java/org/sosoburger/views/UploadedPictures.java
+++ front/src/main/java/org/sosoburger/views/UploadedPictures.java
@@ -1,12 +1,14 @@
 package org.sosoburger.views;
 
-import com.vaadin.flow.component.grid.ColumnTextAlign;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.virtuallist.VirtualList;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import org.json.JSONObject;
 import org.sosoburger.api.PictureApiImpl;
@@ -18,13 +20,46 @@ import java.io.IOException;
 @Route("uploaded")
 public class UploadedPictures extends VerticalLayout {
 
-    private final Grid<PictureDTO> grid = new Grid<>();
     PictureApiImpl pictureApi = new PictureApiImpl();
+
+    VirtualList<PictureDTO> pictureList = new VirtualList<>();
+
     Div text = new Div();
+
+    private final ComponentRenderer<Component, PictureDTO> pictureRenderer = new ComponentRenderer<>(
+            picture -> {
+                VerticalLayout pictureLayout = new VerticalLayout();
+                pictureLayout.setMargin(true);
+                pictureLayout.setAlignItems(Alignment.CENTER);
+
+                Image image = new Image(picture.getPictureURL(), "https://i.imgur.com/UWzk1sl.png");
+                pictureLayout.add(image);
+
+                Grid<TagDTO> tagGrid = new Grid<>();
+                tagGrid.setMinWidth("256px");
+                tagGrid.setAllRowsVisible(true);
+
+                tagGrid.setItems(picture.getTags());
+                tagGrid.addComponentColumn(tagDTO -> new Label(tagDTO.getTag().getRu()))
+                        .setHeader("Тег")
+                        .setComparator(tagDTO -> tagDTO.getTag().getRu()).setAutoWidth(true);
+                tagGrid.addComponentColumn(tagDTO -> new Label(tagDTO.getConfidence().intValue() + "%"))
+                        .setHeader("Точность").setComparator(TagDTO::getConfidence).setAutoWidth(true);
+
+                Details details = new Details("Теги", tagGrid);
+                details.getSummary().getStyle().set("font-size", "30px");
+                details.getSummary().getStyle().set("color", "blue");
+
+                pictureLayout.add(details);
+                return pictureLayout;
+            });
 
     public UploadedPictures() {
         setSizeFull();
-        add(grid, text);
+        setAlignItems(Alignment.CENTER);
+
+        pictureList.setRenderer(pictureRenderer);
+        add(pictureList, text);
         setUploadedPictures();
     }
 
@@ -32,35 +67,8 @@ public class UploadedPictures extends VerticalLayout {
         text.removeAll();
         try {
             var response = pictureApi.getAll().execute();
-
             if (response.isSuccessful()) {
-                grid.setItems(response.body());
-
-                grid.addComponentColumn(picture ->
-                                new Scroller(
-                                new Image(
-                                picture.getPictureURL(),
-                                "Не удалось загрузить изображение")
-                        ))
-                        .setHeader("Изображение")
-                        .setWidth("512px")
-                        .setTextAlign(ColumnTextAlign.CENTER);
-
-                grid.addComponentColumn(picture -> {
-                    Grid<TagDTO> tagGrid = new Grid<>();
-                    tagGrid.setItems(picture.getTags());
-
-                    tagGrid.addComponentColumn(tagDTO -> new Label(tagDTO.getTag().getRu()))
-                            .setHeader("Тег")
-                            .setComparator(tagDTO -> tagDTO.getTag().getRu());
-                    tagGrid.addComponentColumn(tagDTO -> new Label(tagDTO.getConfidence().intValue()+"%"))
-                            .setHeader("Точность").setComparator(TagDTO::getConfidence);
-
-                    return tagGrid;
-                })
-                        .setHeader("Теги").
-                        setAutoWidth(true)
-                        .setTextAlign(ColumnTextAlign.CENTER);
+                pictureList.setItems(response.body());
             } else {
                 if (response.errorBody() != null) {
                     text.add(new Label(new JSONObject(response.errorBody().string()).getString("message")));
